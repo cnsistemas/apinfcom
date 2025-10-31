b9b02e5ff57c8f5040c7e96d9397832a248a8032
Thiago <contato@cnsistemas.net>
2025-10-31 18:01:33 -0300

    alt

--- src/NFCom/NFComXmlBuilder.php
+++ src/NFCom/NFComXmlBuilder.php
@@ -203,9 +203,9 @@ class NFComXmlBuilder
         $xml .= strlen(self::limparNumeros($cpfcnpj)) == 11 ? '<CPF>' . self::limparNumeros($cpfcnpj) . '</CPF>' : '<CNPJ>' . self::limparNumeros($cpfcnpj) . '</CNPJ>';
         $xml .= '<indIEDest>'.$dados['destinatario']['indIEDest'].'</indIEDest>';
         if($dados['destinatario']['indIEDest'] == 1){
-            $xml . '<IE>' . $dados['destinatario']['ie'] . '</IE>';
+            $xml .= '<IE>' . $dados['destinatario']['ie'] . '</IE>';
         }else if($dados['destinatario']['indIEDest'] == 2){
-            $xml . '<IE>ISENTO</IE>';
+            $xml .= '<IE>ISENTO</IE>';
         }
         $xml .= '<enderDest>';
         $xml .= '<xLgr>' . htmlspecialchars($dados['destinatario']['endereco']) . '</xLgr>';
